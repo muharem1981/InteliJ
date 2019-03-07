c9d3b73fd7244760d5886d0aefb8f54d8b866939
hiqatech <hiqatech@gmail.com>
2019-03-07 12:35:54 +0100

    update reports

--- src/test/java/CukesRunTest.java
+++ src/test/java/CukesRunTest.java
@@ -14,7 +14,7 @@ import java.io.File;
         tags = { "@Smoke", "@Regression" },
         glue = {"classpath:common/cucumber","classpath:common.setup","classpath:products"},
             plugin = {
-            "html:target/cucumber-reports",
+            "html:target/cucumber-reports","json:target/cucumber-reports",
             "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/extent-report.html"
             },monochrome = true
             )
